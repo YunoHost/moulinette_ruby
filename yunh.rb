1f34cd22ee88d55f1578e3008294c383362a3613
Kload <kload@kload.fr>
2012-09-16 16:17:51 +0200

    Initialize yunh cli

--- yunh.rb
+++ yunh.rb
@@ -2,7 +2,7 @@
 # encoding: UTF-8
 
 # Remember list
-# Environment variable : env['PATH_INFO'] 
+# Environment variable : env['PATH_INFO']
 # Arguments passed : ARGV
 # Executed code : `ls -l`
 # Include path : $LOAD_PATH << './lib'
@@ -33,18 +33,18 @@ end
 ### Generic LDAP functions ###
 def ldap_search(base, filter, attrs = "dn")
   begin
-  	search = @ldap.search(:base => base, :attributes => attrs, :filter => filter, :return_result => true)
+    search = @ldap.search(:base => base, :attributes => attrs, :filter => filter, :return_result => true)
   rescue
-  	puts "Error: Wrong search arguments \nfilter: #{filter} \nattributes: #{attrs}"
-   	exit 1
+    puts "Error: Wrong search arguments \nfilter: #{filter} \nattributes: #{attrs}"
+    exit 1
   end
   search.each do |entry|
     attrs.each do |attr|
-	  begin
-	  	puts entry.send(attr.to_s)
-	  rescue
-	  	puts "Notice: Undefined attribute '#{attr}' for #{entry.dn}"    		
-	  end
+      begin
+        puts entry.send(attr.to_s)
+      rescue
+        puts "Notice: Undefined attribute '#{attr}' for #{entry.dn}"
+      end
     end
   end
 
@@ -52,20 +52,20 @@ end
 
 case ARGV[0]
 when "user"
-	case ARGV[1]
-	when "search"
-		if ARGV[2]
-			attributes = ARGV[3] ? ARGV[3].split(",") : "dn"
-			ldap_search("ou=users," + LDAPDOMAIN, ARGV[2], attributes)
-		else
-			puts "Usage: yunh user search <ldap_filter> <attributes>\nExample: yunh user search \"cn=Homer Simpson\" uid,mail"
-			exit 1
-		end
-	else
-		puts "Usage: yunh user search | add"
-		exit 1
-	end
+  case ARGV[1]
+  when "search"
+    if ARGV[2]
+      attributes = ARGV[3] ? ARGV[3].split(",") : "dn"
+      ldap_search("ou=users," + LDAPDOMAIN, ARGV[2], attributes)
+    else
+      puts "Usage: yunh user search <ldap_filter> <attributes>\nExample: yunh user search \"cn=Homer Simpson\" uid,mail"
+      exit 1
+    end
+  else
+    puts "Usage: yunh user search | add"
+    exit 1
+  end
 else
-	puts "Error: Need help? Type 'man yunohost'"
-	exit 1
+  puts "Error: Need help? Type 'man yunohost'"
+  exit 1
 end
