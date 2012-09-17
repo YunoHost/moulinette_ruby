904355f60f4dd6cf106d355e7c3d16aaed81de16
Kload <kload@kload.fr>
2012-09-18 01:37:36 +0200

    user_info Test

--- test.rb
+++ test.rb
@@ -6,10 +6,21 @@ require 'highline/import'	# Password prompt
 LDAPPWD = ask("Enter LDAP admin password:  ") { |q| q.echo = false }
 require 'yunohost/functions'
 require 'pp'
+
+@@user_hash = {
+  "username"  => "toto",
+  "mail"      => "toto@tata.net",
+  "lastname"  => "Tata Titi",
+  "firstname" => "Toto",
+  "password"  => "yayaya"
+}
  
 class TestYunoHostFunctions < Test::Unit::TestCase
 
+  
+
   def test_ldap_search
+
   	result = [{
  		"objectclass" 	=> ["mailAccount", "organizationalRole", "posixAccount", "simpleSecurityObject"],
    		"dn" 	      	=> "cn=admin," + LDAPDOMAIN.chomp
@@ -22,39 +33,52 @@ class TestYunoHostFunctions < Test::Unit::TestCase
   	  	"objectclass" 	=> ["groupOfNames", "top"],
    	  	"dn"		=> "cn=admin,ou=groups," + LDAPDOMAIN.chomp
    	  }]
+
   	assert_equal(result, @@yunoldap.search(LDAPDOMAIN, "(cn=admin)", ["dn", "objectclass"], false))
+
   end
-  
  
   def test_user_add
-  	attrs_hash = {
-  		"username"	=> "toto",
- 		"mail"		=> "toto@tata.net",
- 		"lastname"	=> "Tata Titi",
- 		"firstname"	=> "Toto",
- 		"password"	=> "yayaya"
- 	}
 
- 	result = {
- 		:dn=>"cn=Toto Tata Titi,ou=users," + LDAPDOMAIN,
- 		:attrs 	=> {
- 			:objectclass 	=> ["mailAccount", "inetorgperson"],
-		   	:uid 		=> "toto",
-		   	:givenName   	=> "Toto",
-		   	:sn 		=> "Tata Titi",
-		   	:mail 		=> "toto@tata.net",
-		   	:displayName 	=> "Toto Tata Titi",
-		   	:userPassword 	=> "{MD5}odEjpjk2q77oeCPi8a/6mw==",
-		   	:cn 		=> "Toto Tata Titi"
-		 }
- 	}
+   	result = {
+   		:dn	=>"cn=Toto Tata Titi,ou=users," + LDAPDOMAIN,
+   		:attrs 	=> {
+   			:objectclass 	=> ["mailAccount", "inetorgperson"],
+  		   	:uid 		=> "toto",
+  		   	:givenName   	=> "Toto",
+  		   	:sn 		=> "Tata Titi",
+  		   	:mail 		=> "toto@tata.net",
+  		   	:displayName 	=> "Toto Tata Titi",
+  		   	:userPassword 	=> "{MD5}odEjpjk2q77oeCPi8a/6mw==",
+  		   	:cn 		=> "Toto Tata Titi"
+  		 }
+   	}
  	
     user_delete("toto") if @@yunoldap.search(LDAPDOMAIN, "(uid=toto)")
-    assert_equal(result, user_add(attrs_hash))
+    assert_equal(result, user_add(@@user_hash))
+
+  end
+
+  def test_user_info
+
+    result = {
+      "givenName" => "Toto",
+      "mailalias" => "none",
+      "uid"       => "toto",
+      "mail"      => "toto@tata.net",
+      "sn"        => "Tata Titi"
+    }
+
+    user_add(@@user_hash) unless @@yunoldap.search(LDAPDOMAIN, "(uid=toto)")
+    assert_equal(result, user_info("toto"))
+
   end
  
   def test_user_delete
+
+    user_add(@@user_hash) unless @@yunoldap.search(LDAPDOMAIN, "(uid=toto)")
     assert_equal(true, user_delete("toto"))
+    
   end
   
 end
