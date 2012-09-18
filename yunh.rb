a3b12cb2bb6dcecb288f2e4640e31543211873b5
Kload <kload@kload.fr>
2012-09-18 23:40:46 +0200

    Add license & license blocks

--- yunh.rb
+++ yunh.rb
@@ -1,6 +1,22 @@
 #!/usr/bin/env ruby
 # encoding: UTF-8
 
+# YunoHost - Self-hosting for all
+# Copyright (C) 2012  Kload <kload@kload.fr>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 # env['PATH_INFO']
 # $LOAD_PATH << './lib'
 
